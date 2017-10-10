524a53e6ed17d527fb59be9ff20dfa613a8c5796
Oskar Hane <oh@oskarhane.com>
2017-10-10 15:22:50 +0200

    Add rollup and produce bundles
    commonjs
    es6

--- src/Cypher.js
+++ src/Cypher.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import { Component } from "react";
+import * as PropTypes from "prop-types";
 
 export const missingRenderPropError =
   "Pass render property to render something. <Cypher render={({pending, error, response} => {})} />";
